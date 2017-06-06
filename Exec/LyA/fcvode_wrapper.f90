e2d57a2f85f76fe188f956fefdbdadaf789e0165
Brian Friesen <bfriesen@lbl.gov>
2017-06-06 14:43:47 -0700

    LyA: fix bugs in how Fortran and C arrays store data for CVODE solver

--- Exec/LyA/fcvode_wrapper.f90
+++ Exec/LyA/fcvode_wrapper.f90
@@ -1,5 +1,13 @@
 module fcvode_wrapper_mod
+
+  use, intrinsic :: iso_c_binding
   implicit none
+
+  ! Set the number of independent variables -- this should be just "e"
+  integer(c_long), parameter :: neq = 1
+
+  real(c_double) :: yvec(neq)
+
   contains
 
     subroutine fcvode_wrapper(dt, rho_in, T_in, ne_in, e_in, cvmem, sunvec_y, T_out, ne_out, e_out)
@@ -18,11 +26,6 @@ module fcvode_wrapper_mod
         type(c_ptr), value :: sunvec_y
         real(rt), intent(  out) ::         T_out,ne_out,e_out
 
-        ! Set the number of independent variables -- this should be just "e"
-        integer(c_long), parameter :: NEQ = 1
-
-        real(c_double), pointer :: y(:)
-
         real(c_double) :: atol, rtol
         real(c_double) :: time, tout
 
@@ -37,10 +40,8 @@ module fcvode_wrapper_mod
         ! Initialize the integration time
         time = 0.d0
 
-        call N_VGetData_Serial(sunvec_y, neq, y)
-
         ! We will integrate "e" in time. 
-        y(1) = e_in
+        yvec(1) = e_in
 
         ! Set the tolerances.  
         atol = 1.d-4 * e_in
@@ -51,7 +52,7 @@ module fcvode_wrapper_mod
 
         ierr = FCVode(CVmem, dt, sunvec_y, time, CV_NORMAL)
 
-        e_out  = y(1)
+        e_out  = yvec(1)
         T_out  = T_vode
         ne_out = ne_vode
 
