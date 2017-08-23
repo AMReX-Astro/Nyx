7c9919311c53c16c769d84a58b84dc772995eb6b
Brian Friesen <bfriesen@lbl.gov>
2017-08-23 15:59:49 -0700

    LyA: add subroutine fcvode_wrapper_vec()
    This is a SIMD version of the subroutine fcvode_wrapper().

--- Exec/LyA/fcvode_extras.f90
+++ Exec/LyA/fcvode_extras.f90
@@ -55,6 +55,66 @@ module fcvode_extras
 
     end subroutine fcvode_wrapper
 
+    subroutine fcvode_wrapper_vec(dt, rho_in, T_in, ne_in, e_in, neq, cvmem, &
+                              sunvec_y, yvec, T_out, ne_out, e_out)
+
+        use amrex_fort_module, only : rt => amrex_real
+        use vode_aux_module, only: rho_vode_vec, T_vode_vec, ne_vode_vec
+        use cvode_interface
+        use fnvector_serial
+        use misc_params, only: simd_width
+        use, intrinsic :: iso_c_binding
+
+        implicit none
+
+        real(rt), intent(in   ) :: dt
+        real(rt), dimension(simd_width), intent(in   ) :: rho_in, T_in, ne_in, e_in
+        type(c_ptr), value :: cvmem
+        type(c_ptr), value :: sunvec_y
+        real(rt), dimension(simd_width), intent(  out) ::         T_out,ne_out,e_out
+
+        real(c_double) :: rtol
+        real(c_double), pointer, dimension(:) :: atol
+        real(c_double) :: time, tout
+        integer(c_long), intent(in) :: neq
+        real(c_double), pointer, intent(in) :: yvec(:)
+        type(c_ptr) :: sunvec_atol
+
+        integer(c_int) :: ierr
+
+        real(c_double) :: t_soln
+
+        allocate(atol(simd_width))
+
+        sunvec_atol = N_VMake_Serial(neq, atol)
+
+        T_vode_vec(1:simd_width)   = T_in(1:simd_width)
+        ne_vode_vec(1:simd_width)  = ne_in(1:simd_width)
+        rho_vode_vec(1:simd_width) = rho_in(1:simd_width)
+
+        ! Initialize the integration time
+        time = 0.d0
+
+        ! We will integrate "e" in time. 
+        yvec(1:simd_width) = e_in(1:simd_width)
+
+        ! Set the tolerances.  
+        atol(1:simd_width) = 1.d-4 * e_in(1:simd_width)
+        rtol = 1.d-4
+
+        ierr = FCVodeReInit(cvmem, time, sunvec_y)
+        ierr = FCVodeSVtolerances(CVmem, rtol, sunvec_atol)
+
+        ierr = FCVode(CVmem, dt, sunvec_y, time, CV_NORMAL)
+
+        e_out(1:simd_width)  = yvec(1:simd_width)
+        T_out(1:simd_width)  = T_vode_vec(1:simd_width)
+        ne_out(1:simd_width) = ne_vode_vec(1:simd_width)
+
+        deallocate(atol)
+
+    end subroutine fcvode_wrapper_vec
+
     integer(c_int) function RhsFn(tn, sunvec_y, sunvec_f, user_data) &
            result(ierr) bind(C,name='RhsFn')
 
