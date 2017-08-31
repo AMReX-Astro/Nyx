86f97a18121ec4ff8cadd41995e55dfcbe47c8a3
Brian Friesen <bfriesen@lbl.gov>
2017-08-31 16:42:28 -0700

    HeatCool: add SAVE attribute to the SIMD versions of the misc vars in vode_aux
    This way they have the same behavior as their scalar counterparts.

--- Source/HeatCool/vode_aux.f90
+++ Source/HeatCool/vode_aux.f90
@@ -9,7 +9,7 @@ module vode_aux_module
 
   real(rt), save :: z_vode
   real(rt), save :: rho_vode, T_vode, ne_vode
-  real(rt), dimension(:), allocatable :: rho_vode_vec, T_vode_vec, ne_vode_vec
+  real(rt), dimension(:), allocatable, save :: rho_vode_vec, T_vode_vec, ne_vode_vec
   integer , save :: i_vode, j_vode, k_vode
   logical,  save :: firstcall
   !$OMP THREADPRIVATE (rho_vode, rho_vode_vec, T_vode, T_vode_vec, ne_vode, ne_vode_vec, i_vode, j_vode, k_vode, firstcall)
