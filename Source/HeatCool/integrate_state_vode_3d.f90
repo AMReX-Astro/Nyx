20193a1601726fa5927444bcaa8d97ae59883344
Zarija Lukic <zarija@lbl.gov>
2017-10-05 00:53:13 -0700

    heating setup in vode

--- Source/HeatCool/integrate_state_vode_3d.f90
+++ Source/HeatCool/integrate_state_vode_3d.f90
@@ -35,13 +35,13 @@ subroutine integrate_state_vode(lo, hi, &
     use bl_constants_module, only: M_PI
     use eos_params_module
     use network
-    use eos_module, only: nyx_eos_T_given_Re, nyx_eos_given_RT
+    use eos_module, only: nyx_eos_T_given_Re, nyx_eos_given_RT, iterate_ne
     use fundamental_constants_module
     use comoving_module, only: comoving_h, comoving_OmB
     use atomic_rates_module, only: YHELIUM
     use vode_aux_module    , only: JH_vode, JHe_vode, z_vode, i_vode, j_vode, k_vode
     use reion_aux_module   , only: zhi_flash, zheii_flash, flash_h, flash_he, &
-                                   inhomogeneous_on
+                                   T_zhi, T_zheii, inhomogeneous_on
 
     implicit none
 
@@ -54,9 +54,10 @@ subroutine integrate_state_vode(lo, hi, &
     integer         , intent(inout) :: max_iter, min_iter
 
     integer :: i, j, k
-    real(rt) :: z, rho
+    real(rt) :: z, rho, H_reion_z, He_reion_z
     real(rt) :: T_orig, ne_orig, e_orig
-    real(rt) :: T_out , ne_out , e_out, mu, mean_rhob
+    real(rt) :: T_out , ne_out , e_out, mu, mean_rhob, T_H, T_He
+    real(rt) :: species(5)
 
     z = 1.d0/a - 1.d0
 
@@ -74,6 +75,11 @@ subroutine integrate_state_vode(lo, hi, &
        JHe_vode = 1
     endif
 
+    if (flash_h ) H_reion_z  = zhi_flash
+    if (flash_he) He_reion_z = zheii_flash
+
+!print*, "z = ", z, ";  half_dt = ", half_dt
+
     ! Note that (lo,hi) define the region of the box containing the grow cells
     ! Do *not* assume this is just the valid region
     ! apply heating-cooling to UEDEN and UEINT
@@ -88,10 +94,13 @@ subroutine integrate_state_vode(lo, hi, &
                 T_orig  = diag_eos(i,j,k,TEMP_COMP)
                 ne_orig = diag_eos(i,j,k,  NE_COMP)
 
-                if ((inhomogeneous_on) .and. (z .gt. diag_eos(i,j,k,ZHI_COMP))) then
-                    JH_vode = 0
-                else
-                    JH_vode = 1
+                if (inhomogeneous_on) then
+                   H_reion_z = diag_eos(i,j,k,ZHI_COMP)
+                   if (z .gt. H_reion_z) then
+                      JH_vode = 0
+                   else
+                      JH_vode = 1
+                   endif
                 endif
 
                 if (e_orig .lt. 0.d0) then
@@ -118,12 +127,35 @@ subroutine integrate_state_vode(lo, hi, &
                     !call bl_abort('bad e out of strang')
                 end if
 
+                call nyx_eos_T_given_Re(JH_vode, JHe_vode, T_out, ne_out, rho, e_out, a, species)
+
+                !  NOTE: flash case only
+                T_H = 0.0d0
+                if (flash_h .and. (z_vode .lt. H_reion_z)) then
+                   T_H = (1.0d0 - species(2))*T_zhi
+                   flash_h = .false.
+                endif
+
+                T_He = 0.0d0
+                if (flash_he .and. (z_vode .lt. He_reion_z)) then
+                   T_He = (1.0d0 - species(5))*T_zheii
+                   flash_he = .false.
+                endif
+
+                if ((T_H .gt. 0.0d0) .or. (T_He .gt. 0.0d0)) then
+                   T_out = T_orig + T_H + T_He
+                   ne_out = 1.0d0 + YHELIUM
+                   if (T_He .gt. 0.0d0) ne_out = ne_out + YHELIUM
+                   mu = (1.0d0+4.0d0*YHELIUM) / (1.0d0+YHELIUM+ne_out)
+                   e_out  = T_out / (gamma_minus_1 * mp_over_kB * mu)
+                   call nyx_eos_T_given_Re(JH_vode, JHe_vode, T_out, ne_out, rho, e_out, a, species)
+                endif
+
                 ! Update (rho e) and (rho E)
                 state(i,j,k,UEINT) = state(i,j,k,UEINT) + rho * (e_out-e_orig)
                 state(i,j,k,UEDEN) = state(i,j,k,UEDEN) + rho * (e_out-e_orig)
 
                 ! Update T and ne (do not use stuff computed in f_rhs, per vode manual)
-                call nyx_eos_T_given_Re(JH_vode, JHe_vode, T_out, ne_out, rho, e_out, a)
                 diag_eos(i,j,k,TEMP_COMP) = T_out
                 diag_eos(i,j,k,  NE_COMP) = ne_out
 
