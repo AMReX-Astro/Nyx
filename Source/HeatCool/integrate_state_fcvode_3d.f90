dd347c5aa048ae1b63e1eba9e3dcceb28dc2149e
Zarija Lukic <zarija@lbl.gov>
2017-10-05 22:46:57 -0700

    Adjust reionization heating in vode and cvode

--- Source/HeatCool/integrate_state_fcvode_3d.f90
+++ Source/HeatCool/integrate_state_fcvode_3d.f90
@@ -40,10 +40,11 @@ subroutine integrate_state_fcvode(lo, hi, &
     use eos_module, only: nyx_eos_T_given_Re, nyx_eos_given_RT
     use fundamental_constants_module
     use comoving_module, only: comoving_h, comoving_OmB
+    use comoving_nd_module, only: fort_integrate_comoving_a
     use atomic_rates_module, only: YHELIUM
     use vode_aux_module    , only: JH_vode, JHe_vode, z_vode, i_vode, j_vode, k_vode
     use reion_aux_module   , only: zhi_flash, zheii_flash, flash_h, flash_he, &
-                                   inhomogeneous_on
+                                   T_zhi, T_zheii, inhomogeneous_on
     use cvode_interface
     use fnvector_serial
     use fcvode_extras
@@ -60,9 +61,11 @@ subroutine integrate_state_fcvode(lo, hi, &
     integer         , intent(inout) :: max_iter, min_iter
 
     integer :: i, j, k
-    real(rt) :: z, rho
+    real(rt) :: z, z_end, a_end, rho, H_reion_z, He_reion_z
     real(rt) :: T_orig, ne_orig, e_orig
-    real(rt) :: T_out , ne_out , e_out, mu, mean_rhob
+    real(rt) :: T_out , ne_out , e_out, mu, mean_rhob, T_H, T_He
+    real(rt) :: species(5)
+
     integer(c_int) :: ierr       ! error flag from C functions
     real(c_double) :: tstart     ! initial time
     real(c_double) :: atol, rtol
@@ -74,6 +77,8 @@ subroutine integrate_state_fcvode(lo, hi, &
     allocate(yvec(neq))
 
     z = 1.d0/a - 1.d0
+    call fort_integrate_comoving_a(a, a_end, half_dt)
+    z_end = 1.0d0/a_end - 1.0d0
 
     mean_rhob = comoving_OmB * 3.d0*(comoving_h*100.d0)**2 / (8.d0*M_PI*Gconst)
 
@@ -88,6 +93,9 @@ subroutine integrate_state_fcvode(lo, hi, &
     else
        JHe_vode = 1
     endif
+
+    if (flash_h ) H_reion_z  = zhi_flash
+    if (flash_he) He_reion_z = zheii_flash
 
     ! Note that (lo,hi) define the region of the box containing the grow cells
     ! Do *not* assume this is just the valid region
@@ -135,10 +143,13 @@ subroutine integrate_state_fcvode(lo, hi, &
                 T_orig  = diag_eos(i,j,k,TEMP_COMP)
                 ne_orig = diag_eos(i,j,k,  NE_COMP)
 
-                if ((inhomogeneous_on) .and. (z .gt. diag_eos(i,j,k,ZHI_COMP))) then
-                    JH_vode = 0
-                else
-                    JH_vode = 1
+                if (inhomogeneous_on) then
+                   H_reion_z = diag_eos(i,j,k,ZHI_COMP)
+                   if (z .gt. H_reion_z) then
+                      JH_vode = 0
+                   else
+                      JH_vode = 1
+                   endif
                 endif
 
                 if (e_orig .lt. 0.d0) then
@@ -163,12 +174,34 @@ subroutine integrate_state_fcvode(lo, hi, &
 !                    call bl_abort('bad e out of strang')
                 end if
 
+                ! Update T and ne (do not use stuff computed in f_rhs, per vode manual)
+                call nyx_eos_T_given_Re(JH_vode, JHe_vode, T_out, ne_out, rho, e_out, a, species)
+
+                !  Flash heating in reionization:
+                T_H = 0.0d0
+                if (inhomogeneous_on .or. flash_h) then
+                   if ((H_reion_z  .lt. z) .and. (H_reion_z  .ge. z_end)) T_H  = (1.0d0 - species(2))*T_zhi
+                endif
+
+                T_He = 0.0d0
+                if (flash_he) then
+                   if ((He_reion_z .lt. z) .and. (He_reion_z .ge. z_end)) T_He = (1.0d0 - species(5))*T_zheii
+                endif
+
+                if ((T_H .gt. 0.0d0) .or. (T_He .gt. 0.0d0)) then
+                   T_out = T_orig + T_H + T_He
+                   ne_out = 1.0d0 + YHELIUM
+                   if (T_He .gt. 0.0d0) ne_out = ne_out + YHELIUM
+                   mu = (1.0d0+4.0d0*YHELIUM) / (1.0d0+YHELIUM+ne_out)
+                   e_out  = T_out / (gamma_minus_1 * mp_over_kB * mu)
+                   call nyx_eos_T_given_Re(JH_vode, JHe_vode, T_out, ne_out, rho, e_out, a, species)
+                endif
+
                 ! Update (rho e) and (rho E)
                 state(i,j,k,UEINT) = state(i,j,k,UEINT) + rho * (e_out-e_orig)
                 state(i,j,k,UEDEN) = state(i,j,k,UEDEN) + rho * (e_out-e_orig)
 
-                ! Update T and ne (do not use stuff computed in f_rhs, per vode manual)
-                call nyx_eos_T_given_Re(JH_vode, JHe_vode, T_out, ne_out, rho, e_out, a)
+                ! Update T and ne
                 diag_eos(i,j,k,TEMP_COMP) = T_out
                 diag_eos(i,j,k,  NE_COMP) = ne_out
 
