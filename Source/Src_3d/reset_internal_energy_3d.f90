350daadd7d6db36ddd5508206c9d9eba1f57bdb9
Ann Almgren <asalmgren@lbl.gov>
2018-05-09 14:33:39 -0700

    Updated reset_internal_energy that collects the reset_source to be used in SDC algorithm

--- Source/Src_3d/reset_internal_energy_3d.f90
+++ Source/Src_3d/reset_internal_energy_3d.f90
@@ -1,6 +1,7 @@
-      subroutine reset_internal_e(lo,hi,&
+      subroutine reset_internal_e(lo,hi, &
                                   u,u_l1,u_l2,u_l3,u_h1,u_h2,u_h3, &
-                                  d,d_l1,d_l2,d_l3,d_h1,d_h2,d_h3,&
+                                  d,d_l1,d_l2,d_l3,d_h1,d_h2,d_h3, &
+                                  r,r_l1,r_l2,r_l3,r_h1,r_h2,r_h3, &
                                   print_fortran_warnings, comoving_a) &
                                   bind(C, name="reset_internal_e")
 
@@ -16,8 +17,10 @@
       integer          :: print_fortran_warnings
       integer          :: u_l1,u_l2,u_l3,u_h1,u_h2,u_h3
       integer          :: d_l1,d_l2,d_l3,d_h1,d_h2,d_h3
+      integer          :: r_l1,r_l2,r_l3,r_h1,r_h2,r_h3
       real(rt) :: u(u_l1:u_h1,u_l2:u_h2,u_l3:u_h3,NVAR)
       real(rt) :: d(d_l1:d_h1,d_l2:d_h2,d_l3:d_h3,NDIAG)
+      real(rt) :: r(r_l1:r_h1,r_l2:r_h2,r_l3:r_h3)
 
       real(rt), intent(in   ) :: comoving_a
 
@@ -42,11 +45,15 @@
            ! Reset (e from e) if it's greater than 0.01% of big E.
            if (rho_eint .gt. 0.d0 .and. rho_eint / u(i,j,k,UEDEN) .gt. 1.d-6) then
 
+              ! Create reset source so u(i,j,k,UEINT) = u(i,j,k,UEINT) + r(i,j,k) = rho_eint
+               r(i,j,k) = rho_eint - u(i,j,k,UEINT)
                u(i,j,k,UEINT) = rho_eint
 
            ! If (e from E) < 0 or (e from E) < .0001*E but (e from e) > 0.
            else if (u(i,j,k,UEINT) .gt. 0.d0) then
 
+              ! e is not updated, so reset source is zero
+              r(i,j,k) = 0.d0
               u(i,j,k,UEDEN) = u(i,j,k,UEINT) + ke
 
            ! If not resetting and little e is negative ...
@@ -63,11 +70,16 @@
                  call flush(6)
               end if
 
+              ! Create reset source so u(i,j,k,UEINT) = u(i,j,k,UEINT) + r(i,j,k) = u(i,j,k,URHO) * eint_new
+              r(i,j,k) = u(i,j,k,URHO) *  eint_new - u(i,j,k,UEINT)
               u(i,j,k,UEINT) = u(i,j,k,URHO) *  eint_new
 
               u(i,j,k,UEDEN) = u(i,j,k,UEINT) + ke
 
            end if
+
+           ! Scale reset source by 1/rho so src is in terms of e
+!           r(i,j,k) = r(i,j,k) / u(i,j,k,URHO)
 
       enddo
       enddo
