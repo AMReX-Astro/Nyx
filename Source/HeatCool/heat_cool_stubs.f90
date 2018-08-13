b812b4c54f5d1c5a941974ba96b4b345e02056ad
Wolfram Schmidt <wolfram.schmidt@uni-hamburg.de>
2018-08-13 18:28:09 +0200

    Development (#38)
    * Added some more stuff to .gitignore.
    
    * Fixed calls of fort_interp_to_this_z in external sources if nyx.heat_cool_type = 0. Added 32.nyx to LyA for regression testing.
    
    * Cleaning up in DrivenTurbulence and TurbForce.

--- Source/HeatCool/heat_cool_stubs.f90
+++ Source/HeatCool/heat_cool_stubs.f90
@@ -1,18 +1,20 @@
-subroutine integrate_state(lo, hi, state, state_l1, state_l2, &
-                           state_l3, state_h1, state_h2, state_h3, &
-                           dx, time, a, half_dt, min_iter, max_iter) bind(C, name="integrate_state")
+subroutine integrate_state(lo, hi, &
+                           state   , s_l1, s_l2, s_l3, s_h1, s_h2, s_h3, &
+                           diag_eos, d_l1, d_l2, d_l3, d_h1, d_h2, d_h3, &
+                           a, half_dt, min_iter, max_iter) &
+                           bind(C, name="integrate_state")
 
     use amrex_fort_module, only : rt => amrex_real
-    use meth_params_module, only : NVAR
+    use meth_params_module, only : NVAR, NDIAG
 
     implicit none
 
     integer, intent(in) :: lo(3), hi(3)
-    integer, intent(in) :: state_l1, state_l2, state_l3
-    integer, intent(in) :: state_h1, state_h2, state_h3
-    real(rt), intent(inout) :: state(state_l1:state_h1, state_l2:state_h2, &
-                                             state_l3:state_h3, NVAR)
-    real(rt), intent(in   ) :: dx(3), time, a, half_dt
+    integer, intent(in   ) :: s_l1, s_l2, s_l3, s_h1, s_h2, s_h3
+    integer, intent(in   ) :: d_l1, d_l2, d_l3, d_h1, d_h2, d_h3
+    real(rt), intent(in   ) ::    state(s_l1:s_h1, s_l2:s_h2,s_l3:s_h3, NVAR)
+    real(rt), intent(inout) :: diag_eos(d_l1:d_h1, d_l2:d_h2,d_l3:d_h3, NDIAG)
+    real(rt), intent(in   ) :: a, half_dt
     integer,  intent(inout) :: min_iter, max_iter
 
 end subroutine integrate_state
