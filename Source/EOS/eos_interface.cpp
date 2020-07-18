12986bc163a0c3aa0d8339c774b39a69c5c21623
Jean M. Sexton <jmsexton@lbl.gov>
2020-07-18 08:38:19 -0700

    Update eos_given_RT

--- Source/EOS/eos_interface.cpp
+++ Source/EOS/eos_interface.cpp
@@ -17,6 +17,7 @@ extern "C"
 #endif
 */
 
+
 AMREX_GPU_DEVICE void Nyx::nyx_eos_T_given_Re_device(Real gamma_minus_1, Real h_species, int JH, int JHe, Real* T, Real* Ne, Real R,Real e,Real comoving_a)
 {
 
@@ -154,7 +155,13 @@ AMREX_GPU_DEVICE void Nyx::nyx_eos_given_RT(Real gamma_minus_1, Real h_species, 
   *P = ( gamma_minus_1 * den_eos * (*e) ) * comoving_a_cubed;;
 
 #else
-  fort_nyx_eos_given_RT(e,P,R,T,Ne,comoving_a);
+  Real mu = (1.0+4.0*YHELIUM) / (1.0+YHELIUM+Ne);
+  *e = T / (gamma_minus_1 * mp_over_kb * mu);
+  *P  = gamma_minus_1 * (R) * (*e);
+  //  fort_nyx_eos_given_RT(e,P,R,T,Ne,comoving_a);
+  ////  printf("etmp: %g Ptmp: %g\ne:    %g P:    %g\n",e_tmp,P_tmp,*e,*P);
+	 //  if((*e!=e_tmp)&&(*P!=P_tmp))
+  ////    amrex::Abort("found diff");//+e_tmp+P_tmp+(*e)+(*P));
 #endif
 
 }
