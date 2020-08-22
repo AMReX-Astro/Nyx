a4bd73164b6eb190012599cb544846d5067b80c9
Jean M. Sexton <jmsexton@lbl.gov>
2020-08-22 13:17:17 -0700

    Update do_enforce_minimum_density to use small_temp reset, move to update_state file

--- Source/update_state_with_sources.cpp
+++ Source/update_state_with_sources.cpp
@@ -6,6 +6,52 @@ using namespace amrex;
 
 using std::string;
 
+AMREX_GPU_DEVICE
+AMREX_FORCE_INLINE
+void
+do_enforce_minimum_density(
+  const int i,
+  const int j,
+  const int k,
+  amrex::Array4<amrex::Real> const& state,
+  const int NumSpec,
+  amrex::Real const a_new,
+  const amrex::Real gamma_minus_1,
+  const amrex::Real small_dens,
+  const amrex::Real small_temp)
+{
+    //Reset negative density to small_dens and zero out momenta:
+    if(state(i,j,k,URHO) < small_dens)
+    {
+        for (int n = 0; n < NumSpec; ++n)
+        {
+            state(i,j,k,FirstSpec+n) *= small_dens / state(i,j,k,URHO);
+        }
+
+        state(i,j,k,URHO ) = small_dens;
+
+        state(i,j,k,UMX) = 0.0;
+        state(i,j,k,UMY) = 0.0;
+        state(i,j,k,UMZ) = 0.0;
+
+        amrex::Real dummy_pres = 1e200;
+        amrex::Real eint_new = -1e200;
+        amrex::Real Ne = 0.0;
+        amrex::Real h_species = 0.76;
+
+        // HACK HACK -- we can't yet do this call
+        // Re-create "e" from {small_dens, small_temp}
+        nyx_eos_given_RT(gamma_minus_1, h_species, &eint_new, &dummy_pres, state(i,j,k,URHO),
+						 small_temp, Ne,a_new);
+
+        // Define (rho e) with small_dens and the new "e" 
+        state(i,j,k,UEINT) = state(i,j,k,URHO) *  eint_new;
+
+        // Here we effectively zero the KE so set (rho E) = (rho e)
+        state(i,j,k,UEDEN) = state(i,j,k,UEINT);
+    }
+}
+
 void
 Nyx::update_state_with_sources( MultiFab& S_old, MultiFab& S_new,
                                 MultiFab& ext_src_old, MultiFab& hydro_source,
@@ -29,6 +75,8 @@ Nyx::update_state_with_sources( MultiFab& S_old, MultiFab& S_new,
 
     int lnum_spec    = NumSpec;
     Real lsmall_dens = small_dens;
+	Real lgamma_minus_1 = gamma - 1.0;
+	Real lsmall_temp = small_temp;
 
         ////This set of dt should be used for Saxpy dt like setup
     for (amrex::MFIter mfi(S_new, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) 
@@ -73,7 +121,7 @@ Nyx::update_state_with_sources( MultiFab& S_old, MultiFab& S_new,
         //Unclear whether this should be part of previous ParallelFor
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
         {
-                           do_enforce_minimum_density(i, j, k, uout, lnum_spec, lsmall_dens);
+			do_enforce_minimum_density(i, j, k, uout, lnum_spec, a_new, lgamma_minus_1, lsmall_dens, lsmall_temp);
         });
 
     }
